kbarber/ghprb

Big refactoring

src/main/java/org/jenkinsci/plugins/ghprb/GhprbGitHub.java
@@ -1,0 +1,65 @@
+package org.jenkinsci.plugins.ghprb;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+
+/**
+ * @author janinko
+ */
+public class GhprbGitHub {
+	private GitHub gh;
+	private GhprbTrigger trigger;
+	private String gitHubServer;
+
+	public GhprbGitHub(GhprbTrigger trigger, String gitHubServer){
+		this.trigger = trigger;
+		this.gitHubServer = gitHubServer;
+	}
+
+	private void connect() throws IOException{
+		String accessToken = trigger.getDescriptor().getAccessToken();
+		String serverAPIUrl = trigger.getDescriptor().getServerAPIUrl();
+		if(accessToken != null && !accessToken.isEmpty()) {
+			try {
+				gh = GitHub.connectUsingOAuth(serverAPIUrl, accessToken);
+			} catch(IOException e) {
+				Logger.getLogger(GhprbRepo.class.getName()).log(Level.SEVERE, "Can''t connect to {0} using oauth", serverAPIUrl);
+				throw e;
+			}
+		} else {
+			gh = GitHub.connect(trigger.getDescriptor().getUsername(), null, trigger.getDescriptor().getPassword());
+		}
+	}
+
+	public GitHub get() throws IOException{
+		if(gh == null){
+			connect();
+		}
+		return gh;
+	}
+
+	public String getGitHubServer(){
+		return gitHubServer;
+	}
+
+	public boolean isUserMemberOfOrganization(String organisation, String member){
+		try {
+			GHOrganization org = get().getOrganization(organisation);
+			List<GHUser> members = org.getMembers();
+			for(GHUser user : members){
+				if(user.getLogin().equals(member)){
+					return true;
+				}
+			}
+		} catch (IOException ex) {
+			Logger.getLogger(GhprbRepo.class.getName()).log(Level.SEVERE, null, ex);
+			return false;
+		}
+		return false;
+	}
+}

src/main/java/org/jenkinsci/plugins/ghprb/GhprbPullRequest.java
@@ -17,42 +17,46 @@ public class GhprbPullRequest{
 	private Date updated;
 	private String head;
 	private boolean mergeable;
+	private String reponame;
 
 	private boolean shouldRun = false;
 	private boolean accepted = false;
 	@Deprecated private transient boolean askedForApproval; // TODO: remove
 
+	private transient GhprbHelper helper;
 	private transient GhprbRepo repo;
 
-	GhprbPullRequest(GHPullRequest pr, GhprbRepo ghprbRepo) {
+	GhprbPullRequest(GHPullRequest pr) {
 		id = pr.getNumber();
 		updated = pr.getUpdatedAt();
 		head = pr.getHead().getSha();
 		author = pr.getUser().getLogin();
 
-		repo = ghprbRepo;
+		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Created pull request #{0} on {1} by {2} updated at: {3} SHA: {4}", new Object[]{id, reponame, author, updated, head});
+	}
 
-		if(repo.isWhitelisted(author)){
+	public void init(GhprbHelper helper, GhprbRepo repo) {
+		this.helper = helper;
+		this.repo = repo;
+
+		if(helper.isWhitelisted(author)){
 			accepted = true;
 			shouldRun = true;
 		}else{
-			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Author of #{0} {1} on {2} not in whitelist!", new Object[]{id, author, ghprbRepo.getName()});
-			addComment(repo.getDefaultComment());
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Author of #{0} {1} on {2} not in whitelist!", new Object[]{id, author, reponame});
+			repo.addComment(id, helper.getDefaultComment());
 		}
-
-		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Created pull request #{0} on {1} by {2} updated at: {3} SHA: {4}", new Object[]{id, ghprbRepo.getName(), author, updated, head});
 	}
 
-	public void check(GHPullRequest pr, GhprbRepo ghprbRepo){
-		repo = ghprbRepo;
+	public void check(GHPullRequest pr){
 		if(isUpdated(pr)){
-			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request builder: pr #{0} was updated on {1} at {2}", new Object[]{id, ghprbRepo.getName(), updated});
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request builder: pr #{0} was updated on {1} at {2}", new Object[]{id, reponame, updated});
 
 			int commentsChecked = checkComments(pr);
 			boolean newCommit   = checkCommit(pr.getHead().getSha());
 
 			if(!newCommit && commentsChecked == 0){
-				Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request was updated on repo " + ghprbRepo.getName() + " but there aren't any new comments nor commits - that may mean that commit status was updated.");
+				Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request was updated on repo {0} but there aren''t any new comments nor commits - that may mean that commit status was updated.", reponame);
 			}
 			updated = pr.getUpdatedAt();
 		}
@@ -71,28 +75,13 @@ public class GhprbPullRequest{
 		return ret;
 	}
 
-	private void build() {
+	private void build(){
 		shouldRun = false;
+		String message = helper.build(this);
 
-		StringBuilder sb = new StringBuilder();
-		if(repo.cancelBuild(id)){
-			sb.append("Previous build stopped.");
-		}
+		repo.createCommitStatus(head, GHCommitState.PENDING, null, message,id);
 
-		if(mergeable){
-			sb.append(" Merged build triggered.");
-		}else{
-			sb.append(" Build triggered.");
-		}
-
-		repo.startJob(id,head, mergeable);
-		repo.createCommitStatus(head, GHCommitState.PENDING, null, sb.toString(),id);
-
-		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, sb.toString());
-	}
-
-	private void addComment(String comment) {
-		repo.addComment(id,comment);
+		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, message);
 	}
 
 	// returns false if no new commit
@@ -100,7 +89,7 @@ public class GhprbPullRequest{
 		if(head.equals(sha)) return false;
 
 		if(Logger.getLogger(GhprbPullRequest.class.getName()).isLoggable(Level.FINE)){
-			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.FINE, "New commit. Sha: " + head + " => " + sha);
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.FINE, "New commit. Sha: {0} => {1}", new Object[]{head, sha});
 		}
 
 		head = sha;
@@ -112,31 +101,31 @@ public class GhprbPullRequest{
 
 	private void checkComment(GHIssueComment comment) throws IOException {
 		String sender = comment.getUser().getLogin();
-		if (repo.isMe(sender)){
+		if (helper.isMe(sender)){
 			return;
 		}
 		String body = comment.getBody();
 
 		// add to whitelist
-		if (repo.isWhitelistPhrase(body) && repo.isAdmin(sender)){
-			if(!repo.isWhitelisted(author)) {
-				repo.addWhitelist(author);
+		if (helper.isWhitelistPhrase(body) && helper.isAdmin(sender)){
+			if(!helper.isWhitelisted(author)) {
+				helper.addWhitelist(author);
 			}
 			accepted = true;
 			shouldRun = true;
 		}
 
 		// ok to test
-		if(repo.isOktotestPhrase(body) && repo.isAdmin(sender)){
+		if(helper.isOktotestPhrase(body) && helper.isAdmin(sender)){
 			accepted = true;
 			shouldRun = true;
 		}
 
 		// test this please
-		if (repo.isRetestPhrase(body)){
-			if(repo.isAdmin(sender)){
+		if (helper.isRetestPhrase(body)){
+			if(helper.isAdmin(sender)){
 				shouldRun = true;
-			}else if(accepted && repo.isWhitelisted(sender) ){
+			}else if(accepted && helper.isWhitelisted(sender) ){
 				shouldRun = true;
 			}
 		}
@@ -183,5 +172,17 @@ public class GhprbPullRequest{
 		int hash = 7;
 		hash = 89 * hash + this.id;
 		return hash;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getHead() {
+		return head;
+	}
+
+	public boolean isMergeable() {
+		return mergeable;
 	}
 }

jenkinsci/ghprb-plugin

src/main/java/org/jenkinsci/plugins/ghprb/GhprbGitHub.java
@@ -1,0 +1,63 @@
+package org.jenkinsci.plugins.ghprb;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+
+/**
+ * @author janinko
+ */
+public class GhprbGitHub {
+	private GitHub gh;
+	private String gitHubServer;
+
+	public GhprbGitHub(String gitHubServer){
+		this.gitHubServer = gitHubServer;
+	}
+
+	private void connect() throws IOException{
+		String accessToken = GhprbTrigger.getDscp().getAccessToken();
+		String serverAPIUrl = GhprbTrigger.getDscp().getServerAPIUrl();
+		if(accessToken != null && !accessToken.isEmpty()) {
+			try {
+				gh = GitHub.connectUsingOAuth(serverAPIUrl, accessToken);
+			} catch(IOException e) {
+				Logger.getLogger(GhprbRepository.class.getName()).log(Level.SEVERE, "Can''t connect to {0} using oauth", serverAPIUrl);
+				throw e;
+			}
+		} else {
+			gh = GitHub.connect(GhprbTrigger.getDscp().getUsername(), null, GhprbTrigger.getDscp().getPassword());
+		}
+	}
+
+	public GitHub get() throws IOException{
+		if(gh == null){
+			connect();
+		}
+		return gh;
+	}
+
+	public String getGitHubServer(){
+		return gitHubServer;
+	}
+
+	public boolean isUserMemberOfOrganization(String organisation, String member){
+		try {
+			GHOrganization org = get().getOrganization(organisation);
+			List<GHUser> members = org.getMembers();
+			for(GHUser user : members){
+				if(user.getLogin().equals(member)){
+					return true;
+				}
+			}
+		} catch (IOException ex) {
+			Logger.getLogger(GhprbRepository.class.getName()).log(Level.SEVERE, null, ex);
+			return false;
+		}
+		return false;
+	}
+}

src/main/java/org/jenkinsci/plugins/ghprb/GhprbPullRequest.java
@@ -17,48 +17,68 @@ public class GhprbPullRequest{
 	private Date updated;
 	private String head;
 	private boolean mergeable;
+	private String reponame;
 
 	private boolean shouldRun = false;
 	private boolean accepted = false;
 	@Deprecated private transient boolean askedForApproval; // TODO: remove
 
-	private transient GhprbRepo repo;
+	private transient Ghprb ml;
+	private transient GhprbRepository repo;
 
-	GhprbPullRequest(GHPullRequest pr, GhprbRepo ghprbRepo) {
+	GhprbPullRequest(GHPullRequest pr, Ghprb helper, GhprbRepository repo) {
 		id = pr.getNumber();
 		updated = pr.getUpdatedAt();
 		head = pr.getHead().getSha();
 		author = pr.getUser().getLogin();
 
-		repo = ghprbRepo;
+		this.ml = helper;
+		this.repo = repo;
 
-		if(repo.isWhitelisted(author)){
+		if(helper.isWhitelisted(author)){
 			accepted = true;
 			shouldRun = true;
 		}else{
-			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Author of #{0} {1} on {2} not in whitelist!", new Object[]{id, author, ghprbRepo.getName()});
-			addComment(repo.getDefaultComment());
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Author of #{0} {1} on {2} not in whitelist!", new Object[]{id, author, reponame});
+			repo.addComment(id, GhprbTrigger.getDscp().getRequestForTestingPhrase());
 		}
 
-		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Created pull request #{0} on {1} by {2} updated at: {3} SHA: {4}", new Object[]{id, ghprbRepo.getName(), author, updated, head});
+		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Created pull request #{0} on {1} by {2} updated at: {3} SHA: {4}", new Object[]{id, reponame, author, updated, head});
 	}
 
-	public void check(GHPullRequest pr, GhprbRepo ghprbRepo){
-		repo = ghprbRepo;
+	public void init(Ghprb helper, GhprbRepository repo) {
+		this.ml = helper;
+		this.repo = repo;
+	}
+
+	public void check(GHPullRequest pr){
 		if(isUpdated(pr)){
-			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request builder: pr #{0} was updated on {1} at {2}", new Object[]{id, ghprbRepo.getName(), updated});
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request builder: pr #{0} was updated on {1} at {2}", new Object[]{id, reponame, updated});
 
 			int commentsChecked = checkComments(pr);
 			boolean newCommit   = checkCommit(pr.getHead().getSha());
 
 			if(!newCommit && commentsChecked == 0){
-				Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request was updated on repo " + ghprbRepo.getName() + " but there aren't any new comments nor commits - that may mean that commit status was updated.");
+				Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, "Pull request was updated on repo {0} but there aren''t any new comments nor commits - that may mean that commit status was updated.", reponame);
 			}
 			updated = pr.getUpdatedAt();
 		}
 
 		if(shouldRun){
 			checkMergeable(pr);
+			build();
+		}
+	}
+
+	public void check(GHIssueComment comment) {
+		try {
+			checkComment(comment);
+			updated = comment.getUpdatedAt();
+		} catch (IOException ex) {
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.SEVERE, "Couldn't check comment #" + comment.getId(), ex);
+			return;
+		}
+		if (shouldRun) {
 			build();
 		}
 	}
@@ -71,28 +91,13 @@ public class GhprbPullRequest{
 		return ret;
 	}
 
-	private void build() {
+	private void build(){
 		shouldRun = false;
+		String message = ml.getBuilds().build(this);
 
-		StringBuilder sb = new StringBuilder();
-		if(repo.cancelBuild(id)){
-			sb.append("Previous build stopped.");
-		}
+		repo.createCommitStatus(head, GHCommitState.PENDING, null, message,id);
 
-		if(mergeable){
-			sb.append(" Merged build triggered.");
-		}else{
-			sb.append(" Build triggered.");
-		}
-
-		repo.startJob(id,head, mergeable);
-		repo.createCommitStatus(head, GHCommitState.PENDING, null, sb.toString(),id);
-
-		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, sb.toString());
-	}
-
-	private void addComment(String comment) {
-		repo.addComment(id,comment);
+		Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.INFO, message);
 	}
 
 	// returns false if no new commit
@@ -100,7 +105,7 @@ public class GhprbPullRequest{
 		if(head.equals(sha)) return false;
 
 		if(Logger.getLogger(GhprbPullRequest.class.getName()).isLoggable(Level.FINE)){
-			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.FINE, "New commit. Sha: " + head + " => " + sha);
+			Logger.getLogger(GhprbPullRequest.class.getName()).log(Level.FINE, "New commit. Sha: {0} => {1}", new Object[]{head, sha});
 		}
 
 		head = sha;
@@ -112,31 +117,28 @@ public class GhprbPullRequest{
 
 	private void checkComment(GHIssueComment comment) throws IOException {
 		String sender = comment.getUser().getLogin();
-		if (repo.isMe(sender)){
-			return;
-		}
 		String body = comment.getBody();
 
 		// add to whitelist
-		if (repo.isWhitelistPhrase(body) && repo.isAdmin(sender)){
-			if(!repo.isWhitelisted(author)) {
-				repo.addWhitelist(author);
+		if (ml.isWhitelistPhrase(body) && ml.isAdmin(sender)){
+			if(!ml.isWhitelisted(author)) {
+				ml.addWhitelist(author);
 			}
 			accepted = true;
 			shouldRun = true;
 		}
 
 		// ok to test
-		if(repo.isOktotestPhrase(body) && repo.isAdmin(sender)){
+		if(ml.isOktotestPhrase(body) && ml.isAdmin(sender)){
 			accepted = true;
 			shouldRun = true;
 		}
 
 		// test this please
-		if (repo.isRetestPhrase(body)){
-			if(repo.isAdmin(sender)){
+		if (ml.isRetestPhrase(body)){
+			if(ml.isAdmin(sender)){
 				shouldRun = true;
-			}else if(accepted && repo.isWhitelisted(sender) ){
+			}else if(accepted && ml.isWhitelisted(sender) ){
 				shouldRun = true;
 			}
 		}
@@ -183,5 +185,17 @@ public class GhprbPullRequest{
 		int hash = 7;
 		hash = 89 * hash + this.id;
 		return hash;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getHead() {
+		return head;
+	}
+
+	public boolean isMergeable() {
+		return mergeable;
 	}
 }