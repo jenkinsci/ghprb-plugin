1459dfcd80844c9e8e76bafafe270a349833b5ca
Honza Brázdil <jbrazdil@redhat.com>
2013-12-13 15:24:17 +0100

    remove unused import

--- src/main/java/org/jenkinsci/plugins/ghprb/GhprbGitHub.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/GhprbGitHub.java
@@ -1,7 +1,6 @@
 package org.jenkinsci.plugins.ghprb;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.kohsuke.github.GHOrganization;
