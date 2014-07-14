e0a7afccafc4f93ea58733658becbba231754f9f
QuinRogers <quin.rogers@metamarkets.com>
2014-07-14 13:01:54 -0700

    Update GhprbBuilds.java

--- src/main/java/org/jenkinsci/plugins/ghprb/GhprbBuilds.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/GhprbBuilds.java
@@ -3,13 +3,21 @@ package org.jenkinsci.plugins.ghprb;
 import hudson.model.AbstractBuild;
 import hudson.model.Cause;
 import hudson.model.Result;
+import hudson.model.User;
 import hudson.model.queue.QueueTaskFuture;
+import hudson.plugins.git.GitChangeSet;
 import hudson.plugins.git.util.BuildData;
+import hudson.scm.ChangeLogSet;
+import hudson.scm.ChangeLogSet.Entry;
+import hudson.tasks.junit.CaseResult;
+import hudson.tasks.test.AbstractTestResultAction;
+import hudson.tasks.test.TestResult;
 import org.kohsuke.github.GHCommitState;
 import org.kohsuke.github.GHIssueState;
 import org.kohsuke.github.GHPullRequest;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +25,8 @@ import java.util.logging.Logger;
 /**
  * @author janinko
  */
+//Editied by Quin Rogers
+
 public class GhprbBuilds {
     private static final Logger logger = Logger.getLogger(GhprbBuilds.class.getName());
     private final GhprbTrigger trigger;
@@ -111,8 +121,66 @@ public class GhprbBuilds {
             } else {
                 msg.append(GhprbTrigger.getDscp().getMsgFailure());
             }
+
             msg.append("\nRefer to this link for build results: ");
             msg.append(publishedURL).append(build.getUrl());
+            msg.append("\nBuild Duration: ");
+            msg.append(build.getDurationString());
+            msg.append("\nBuilt on node: ");
+            msg.append(build.getBuiltOnStr());
+
+            //If the name of the node name was empty, the node was the master
+            if(build.getBuiltOnStr().equals("")){
+                msg.append("Master");
+            }
+
+            AbstractTestResultAction testResults = build.getTestResultAction();
+            Boolean testResultsFound = (testResults != null);
+            Iterator iterator;
+
+            iterator = build.getChangeSet().iterator();
+            msg.append("\n\n");
+            if(iterator.hasNext()){
+                msg.append("Changes in this build:\n");
+                while(iterator.hasNext()){
+                    ChangeLogSet.Entry clse = (ChangeLogSet.Entry)iterator.next();
+                    msg.append(clse.getMsgEscaped()).append(" by ");
+                    msg.append(clse.getAuthor().getDisplayName()).append(" ");
+                    msg.append(clse.getCommitId()).append("\n");
+                }
+            } else {
+                msg.append("No changes in this build");
+            }
+
+            if(state != GHCommitState.SUCCESS){
+                //if the tests fail and there are test restults, display them
+                if(testResultsFound){
+                    msg.append("\n\n=====Failed Test Information=====\n");
+                    msg.append("\n ```\n");
+
+                    List<TestResult> failedTests = testResults.getFailedTests();
+                    iterator = failedTests.iterator();
+
+                    msg.append("\nNumber of failed tests: ").append(testResults.getFailCount());
+                    msg.append("/").append(testResults.getTotalCount());
+                    msg.append("\nNumber of skipped tests: ").append(testResults.getSkipCount());
+
+                    while(iterator.hasNext()){
+
+                        CaseResult cr = (CaseResult)iterator.next();
+
+                        //subString(12) returns the string after the first 12 letters, which are not needed. They are: "Case Result:"
+                        msg.append("\n\nFailed test: ").append(cr.getTitle().substring(12));
+                        msg.append("\nRuntime: ").append(cr.getDuration()).append(" seconds");
+                        msg.append("\nNumber of consecutive failed builds: ").append(cr.getAge());
+                    }
+
+                    msg.append("\n ```\n");
+                } else {
+                    msg.append("\nNo tests results were found");
+                    logger.log(Level.INFO, "Test results could not be found");
+                }
+            }
 
             int numLines = GhprbTrigger.getDscp().getlogExcerptLines();
             if (state != GHCommitState.SUCCESS && numLines > 0) {
