e29dbae81b2e1fdf796c4e9b8a4fbfb93df7d421
Josh Caldwell <jec006@gmail.com>
2014-07-31 10:21:11 -0400

    Added a TokenMacro provider to generate tokens so they can be used within Build Names and other place

---
+++ src/main/java/org/jenkinsci/plugins/ghprb/GhprbTokenMacro.java
@@ -0,0 +1,38 @@
+package org.jenkinsci.plugins.ghprb;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.Cause;
+
+import org.jenkinsci.plugins.tokenmacro.DataBoundTokenMacro;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+
+import java.io.IOException;
+
+/**
+ * {@code PR_Name} token that expands to the PR Name.
+ * {@code PR_User} token that expands to the PR Opener's email.
+ *
+ * @author Josh Caldwell
+ */
+@Extension(optional=true)
+public class GhprbTokenMacro extends DataBoundTokenMacro {
+    @Override
+    public boolean acceptsMacroName(String macroName) {
+        return macroName.equals("PR_Title") || macroName.equals("PR_Email");
+    }
+
+    @Override
+    public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName) throws MacroEvaluationException, IOException, InterruptedException {
+        Cause cause = context.getCause(GhprbCause.class);
+        if (cause == null) {
+            return "";
+        }
+
+        if (macroName.equals("PR_Title")) {
+            return cause.getTitle();
+        } else if (macroName.equals("PR_Email")) {
+            return cause.getAuthorEmail();
+        }
+    }
+}
