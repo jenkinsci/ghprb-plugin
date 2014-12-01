c7008c52e90e51d28312a9edd0057ae7dc0fe7f7
Kanstantsin Shautsou <Kanstantsin_Shautsou@epam.com>
2014-12-01 17:17:39 +0300

    Fix typo in word "wasn't"

--- src/main/java/org/jenkinsci/plugins/ghprb/GhprbTrigger.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/GhprbTrigger.java
@@ -324,7 +324,7 @@ public class GhprbTrigger extends Trigger<AbstractProject<?, ?>> {
 
     public GhprbBuilds getBuilds() {
         if(helper == null) {
-            logger.log(Level.SEVERE, "The ghprb trigger for {0} wasn''t properly started - helper is null", this.project);
+            logger.log(Level.SEVERE, "The ghprb trigger for {0} wasn't properly started - helper is null", this.project);
             return null;
         }
         return helper.getBuilds();
@@ -332,7 +332,7 @@ public class GhprbTrigger extends Trigger<AbstractProject<?, ?>> {
 
     public GhprbRepository getRepository() {
         if(helper == null) {
-            logger.log(Level.SEVERE, "The ghprb trigger for {0} wasn''t properly started - helper is null", this.project);
+            logger.log(Level.SEVERE, "The ghprb trigger for {0} wasn't properly started - helper is null", this.project);
             return null;
         }
         return helper.getRepository();
