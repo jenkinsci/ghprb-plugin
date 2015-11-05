74d70ebcfb821086726ef7721c5ff1a447f10f24
Daniel Spilker <daniel.spilker@coremedia.com>
2015-11-05 19:10:23 +0100

    it's OK for a @DslExtensionMethod to throw an exception

--- src/main/java/org/jenkinsci/plugins/ghprb/jobdsl/GhprbContextExtensionPoint.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/jobdsl/GhprbContextExtensionPoint.java
@@ -3,7 +3,6 @@ package org.jenkinsci.plugins.ghprb.jobdsl;
 import antlr.ANTLRException;
 import com.google.common.base.Joiner;
 import hudson.Extension;
-import javaposse.jobdsl.dsl.DslException;
 import javaposse.jobdsl.dsl.helpers.triggers.TriggerContext;
 import javaposse.jobdsl.plugin.ContextExtensionPoint;
 import javaposse.jobdsl.plugin.DslExtensionMethod;
@@ -15,33 +14,29 @@ import java.util.ArrayList;
 @Extension(optional = true)
 public class GhprbContextExtensionPoint extends ContextExtensionPoint {
     @DslExtensionMethod(context = TriggerContext.class)
-    public Object githubPullRequest(Runnable closure) {
+    public Object githubPullRequest(Runnable closure) throws ANTLRException {
         GhprbTriggerContext context = new GhprbTriggerContext();
         executeInContext(closure, context);
-        try {
-            return new GhprbTrigger(
-                    Joiner.on("\n").join(context.admins),
-                    Joiner.on("\n").join(context.userWhitelist),
-                    Joiner.on("\n").join(context.orgWhitelist),
-                    context.cron,
-                    context.triggerPhrase,
-                    context.onlyTriggerPhrase,
-                    context.useGitHubHooks,
-                    context.permitAll,
-                    context.autoCloseFailedPullRequests,
-                    null,
-                    null,
-                    new ArrayList<GhprbBranch>(),
-                    context.allowMembersOfWhitelistedOrgsAsAdmin,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    context.extensionContext.extensions
-            );
-        } catch (ANTLRException e) {
-            throw new DslException("invalid cron tab", e);
-        }
+        return new GhprbTrigger(
+                Joiner.on("\n").join(context.admins),
+                Joiner.on("\n").join(context.userWhitelist),
+                Joiner.on("\n").join(context.orgWhitelist),
+                context.cron,
+                context.triggerPhrase,
+                context.onlyTriggerPhrase,
+                context.useGitHubHooks,
+                context.permitAll,
+                context.autoCloseFailedPullRequests,
+                null,
+                null,
+                new ArrayList<GhprbBranch>(),
+                context.allowMembersOfWhitelistedOrgsAsAdmin,
+                null,
+                null,
+                null,
+                null,
+                null,
+                context.extensionContext.extensions
+        );
     }
 }
