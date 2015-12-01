5134ded8eb0fe6ec498c4836853f7fb90f8b0c37
Oliver Gondža <ogondza@gmail.com>
2015-12-01 15:25:26 +0100

    [JENKINS-31827] Use default timeouts for URLConnections

--- src/main/java/org/jenkinsci/plugins/ghprb/HttpConnectorWithJenkinsProxy.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/HttpConnectorWithJenkinsProxy.java
@@ -1,14 +1,24 @@
 package org.jenkinsci.plugins.ghprb;
 
-import hudson.ProxyConfiguration;
-import org.kohsuke.github.HttpConnector;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.kohsuke.github.HttpConnector;
+
+import hudson.ProxyConfiguration;
+
 public class HttpConnectorWithJenkinsProxy implements HttpConnector {
     public HttpURLConnection connect(URL url) throws IOException {
-        return (HttpURLConnection) ProxyConfiguration.open(url);
+        HttpURLConnection con = (HttpURLConnection) ProxyConfiguration.open(url);
+
+        // Set default timeouts in case there are none
+        if (con.getConnectTimeout() == 0) {
+            con.setConnectTimeout(10000);
+        }
+        if (con.getReadTimeout() == 0) {
+            con.setReadTimeout(10000);
+        }
+        return con;
     }
 }
