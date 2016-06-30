9c46eab31437d8feaeec7658fcef5062f6b7a40c
David Tanner <david.joel.tanner@gmail.com>
2016-06-30 07:15:59 -0600

    GetCommentsCount always returns 0 so comments never work

--- src/main/java/org/jenkinsci/plugins/ghprb/GhprbPullRequest.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/GhprbPullRequest.java
@@ -418,11 +418,11 @@ public class GhprbPullRequest {
 
     private int checkComments(GHPullRequest ghpr,
                               Date lastUpdatedTime) {
-
-        if (ghpr.getCommentsCount() == 0) {
-            // Avoid the API call. Nothing to do here.
-            return 0;
-        }
+//        It looks like this is always returning 0, ignoring till it can be confirmed.
+//        if (ghpr.getCommentsCount() == 0) {
+//            // Avoid the API call. Nothing to do here.
+//            return 0;
+//        }
 
         int count = 0;
         logger.log(Level.FINEST, "Checking for comments after: {0}", lastUpdatedTime);
