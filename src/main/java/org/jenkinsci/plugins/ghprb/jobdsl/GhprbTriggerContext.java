fc1020242a6e1d92f382ea49b12b1589e6d3d6a1
Björn Häuser <b.haeuser@rebuy.de>
2017-11-19 11:24:47 +0100

    Improve inline docu by a very small bit

--- src/main/java/org/jenkinsci/plugins/ghprb/jobdsl/GhprbTriggerContext.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/jobdsl/GhprbTriggerContext.java
@@ -271,28 +271,29 @@ class GhprbTriggerContext implements Context {
     public void extensions(Runnable closure) {
         ContextExtensionPoint.executeInContext(closure, extensionContext);
     }
- 
-    public void includedRegions(String regions){
-    	this.includedRegions = regions;
+
+    public void includedRegions(String regions) {
+        this.includedRegions = regions;
     }
 
-    public void excludedRegions(String regions){
+    public void excludedRegions(String regions) {
         this.excludedRegions = regions;
     }
-    public void includedRegions(Iterable<String> regions){
-	String includedRegionsStr = "";
-	for (String region: regions) {
-             includedRegionsStr += (region + "\n");    
+
+    public void includedRegions(Iterable<String> regions) {
+        String includedRegionsStr = "";
+        for (String region: regions) {
+             includedRegionsStr += (region + "\n");
         }
-	includedRegions(includedRegionsStr);
+        includedRegions(includedRegionsStr);
     }
-    public void excludedRegions(Iterable<String> regions){
+
+    public void excludedRegions(Iterable<String> regions) {
         String excludedRegionsStr = "";
-	for (String region: regions) {
+        for (String region: regions) {
              excludedRegionsStr += (region+ "\n");
         }
         excludedRegions(excludedRegionsStr);
-
     }
 
 }
